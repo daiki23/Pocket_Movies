6c90d0a57ec3b8ac2f88a51b434cdf08313dfdb8
daiki23 <r.dikaadji@gmail>
2017-05-14 10:31:01 +0700

    aktivasi Upcoming Movie

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl419/pocket_movies/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl419/pocket_movies/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.sch.smktelkom_mlg.privateassignment.xirpl419.pocket_movies.Fragment.UpComingFragment;
 import id.sch.smktelkom_mlg.privateassignment.xirpl419.pocket_movies.adapter.SourceAdapter;
 import id.sch.smktelkom_mlg.privateassignment.xirpl419.pocket_movies.Fragment.AboutFragment;
 import id.sch.smktelkom_mlg.privateassignment.xirpl419.pocket_movies.Fragment.HomeFragment;
@@ -107,7 +108,11 @@ public class MainActivity extends AppCompatActivity
             fragment = new NowFragment();
             setTitle("Now Playing");
 
-        } else if (id == R.id.nav_about) {
+        } else if (id == R.id.nav_up) {
+            fragment = new UpComingFragment();
+            setTitle("Upcoming Movie!");
+
+        }else if (id == R.id.nav_about) {
             fragment = new AboutFragment();
             setTitle("About Author!");
 
